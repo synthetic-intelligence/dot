03ffc48e8717d3b16227f1fc03b2914765f87e02
drsjb80 <drsjb80@gmail.com>
2019-02-10 09:04:30 -0700

    Adding .vimrc

--- fix.sh
+++ fix.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd ~
-FILES='.bash_profile .bashrc bin/choice bin/bash-powerline.sh'
+FILES='.vimrc .bash_profile .bashrc bin/choice bin/bash-powerline.sh'
 rm -f $FILES
 for i in $FILES
 do
