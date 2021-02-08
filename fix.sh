eab15826f150a7d5f55905432faecb6cdb4e018b
Steve Beaty <drsjb80@gmail.com>
2021-02-08 07:41:35 -0700

    Bugs fixes

--- fix.sh
+++ fix.sh
@@ -2,9 +2,21 @@
 
 cd ~
 FILES='.vimrc .bash_profile .bashrc .signatures .screenrc bin/randomsignature bin/choice bin/bash-powerline.sh'
-rm -f $FILES
+mkdir bin 2> /dev/null
+rm -f $FILES 2> /dev/null
+
 for i in $FILES
 do
     echo $i
-    ln -s ~/src/dotfiles/$i $i
+    ln ~/src/dotfiles/$i $i
 done
+
+mkdir ${release}/chrome 2> /dev/null
+release=$(/bin/ls -d ~/.mozilla/firefox/*.default-release)
+rm  ${release}/chrome/userChrome.css 2> /dev/null
+ln ~/src/dotfiles/userChrome.css ${release}/chrome/userChrome.css
+
+mkdir ${release}/chrome 2> /dev/null
+release=$(/bin/ls -d ~/.mozilla/firefox/*.default)
+rm  ${release}/chrome/userChrome.css 2> /dev/null
+ln ~/src/dotfiles/userChrome.css ${release}/chrome/userChrome.css
