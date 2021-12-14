9ec1b4c0d132006aafa21ff99760ec2adb0fb35a
drsjb80 <drsjb80@gmail.com>
2021-12-14 07:43:00 -0700

    Lots of cleanup

--- fix.sh
+++ fix.sh
@@ -1,21 +1,36 @@
 #! /bin/bash
 
+# dconf watch /
+
 FILES=".vimrc .gtkrc-2.0 .prsst.yml .bash_profile .bashrc .signatures .screenrc bin/*"
 
 if [[ $(uname) == Linux ]]
 then
-  for uuid in $(gsettings get org.gnome.Terminal.ProfilesList list | tr -d "',")
-do
+    UUIDS=$(gsettings get org.gnome.Terminal.ProfilesList list | tr -d "',")
+    PALETTE="['rgb(7,54,66)', 'rgb(0,0,0)', 'rgb(85,85,85)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
     A='org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:'
-        gsettings set "${A}${uuid}"/ cursor-blink-mode off
-        gsettings set "${A}${uuid}"/ background-color '#DFDBC3'
-        gsettings set "${A}${uuid}"/ foreground-color '#000000'
-        gsettings set "${A}${uuid}"/ default-size-rows 44
-  done
-  gsettings set org.gnome.desktop.interface cursor-blink false
-  gsettings set org.cinnamon.desktop.keybindings.wm close "['<Super>q']"
-  gsettings set org.cinnamon.desktop.keybindings.wm minimize "['<Super>m']"
-  gsettings set org.cinnamon.desktop.interface gtk-overlay-scrollbars false
+
+    # kinda 2-d array
+    a=(cursor-blink-mode background-color foreground-color palette default-size-rows)
+    b=(off '#DFDBC3' '#000000' "${PALETTE}" 44)
+    for UUID in ${UUIDS}
+    do
+        length=${#a[@]}
+        for (( i=0; i<length; i++ ))
+        do
+            gsettings set "${A}${UUID}"/ "${a[$i]}" "${b[$i]}"
+        done
+    done
+    gsettings set org.gnome.desktop.interface cursor-blink false
+
+    WM=org.cinnamon.desktop.keybindings.wm
+    a=( close minimize move-to-workspace-right move-to-workspace-left )
+    b=( "['<Super>q']" "['<Super>m', '<Super>h']" "['<Super>Right']" "['<Super>Left']" )
+    length=${#a[@]}
+    for (( i=0; i<length; i++ ))
+    do
+        gsettings set "${WM}" "${a[$i]}" "${b[$i]}"
+    done
 fi
 
 mkdir ~/bin 2> /dev/null
@@ -30,7 +45,7 @@ done
 function mozilla_prefs () {
     if ! grep "${1}" "${2}" > /dev/null
     then
-        echo 'user_pref("'"${1}"'", "'"${3}"'");' >> "${PREFS}"
+        echo 'user_pref("'"${1}"'", "'"${3}"'");' >> "${2}"
     fi
 }
 
@@ -81,7 +96,8 @@ TB=( ~/.thunderbird/*.default ~/.thunderbird/*.default.release ~/Library/Thunder
 
 userChrome "${TB[@]}"
 
-# /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/palette
+# gsettings get org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ palette
+
 # foo="['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
 
 
