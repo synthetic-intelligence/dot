109c9bb6d82f426c5395b4b36ba7d12ba80d9e68
Steve Beaty <drsjb80@gmail.com>
2021-12-16 09:19:43 -0700

    Cinnamon conky files and shellcheck fixes.

--- fix.sh
+++ fix.sh
@@ -11,15 +11,24 @@ do
     ln ~/src/dotfiles/"${FILE}" ~/"${FILE}"
 done
 
+mkdir -p ~/.config/autostart 2> /dev/null
+
+for FILE in .config/autostart/*
+do
+    rm -f ~/"${FILE}" 2> /dev/null
+    ln ~/src/dotfiles/"${FILE}" ~/"${FILE}"
+done
+exit
+
 SAVEIFS="${IFS}"
 IFS=$'\n'
 # sigh, see: https://superuser.com/questions/1507251/firefox-has-two-default-profiles-default-release-and-default-which-one-sho
-FF="~/.mozilla/firefox/*.default-release \
-    ~/.mozilla/firefox/*.default \
-    ~/Library/Application\ Support/Firefox/Profiles/*.default-release \
-    ~/Library/Application\ Support/Firefox/Profiles/*.default"
+FF="$HOME/.mozilla/firefox/*.default-release \
+    $HOME/.mozilla/firefox/*.default \
+    $HOME/Library/Application\ Support/Firefox/Profiles/*.default-release \
+    $HOME/Library/Application\ Support/Firefox/Profiles/*.default"
 
-for i in $(/bin/ls -d ${FF} 2> /dev/null)
+for i in $(/bin/ls -d "${FF}" 2> /dev/null)
 do
     DIR="${i}"/chrome
     # echo "DIR: ${DIR}"
@@ -31,12 +40,12 @@ do
     ln ~/src/dotfiles/FFuserChrome.css "${FILE}"
 done
 
-TB="~/.thunderbird/*.default \
-    ~/.thunderbird/*.default.release \
-    ~/Library/Thunderbird/Profiles/*.default \
-    ~/Library/Thunderbird/Profiles/*.default.release"
+TB="$HOME/.thunderbird/*.default \
+    $HOME/.thunderbird/*.default.release \
+    $HOME/Library/Thunderbird/Profiles/*.default \
+    $HOME/Library/Thunderbird/Profiles/*.default.release"
 
-for i in $(/bin/ls -d ${TB} 2> /dev/null)
+for i in $(/bin/ls -d "${TB}" 2> /dev/null)
 do
     DIR="${i}"/chrome
     mkdir -p "${DIR}" 2> /dev/null
@@ -46,15 +55,18 @@ do
     ln ~/src/dotfiles/TBuserChrome.css "${FILE}"
 
     DIR="${i}"/webaccountMail/outlook.office365.com
-    mkdir -p ${DIR} 2> /dev/null
+    mkdir -p "${DIR}" 2> /dev/null
 
     FILE="${DIR}"/msgFilterRules.dat
     rm "${FILE}" 2> /dev/null
     ln ~/src/dotfiles/msgFilterRules.dat "${FILE}"
 
-    if ! grep 'toolkit.legacyUserProfileCustomizations.stylesheets' ${i}/prefs.js > /dev/null
+    if ! grep 'toolkit.legacyUserProfileCustomizations.stylesheets' "${i}"/prefs.js > /dev/null
     then
-        echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> ${i}/prefs.js
+        echo
+        'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> "${i}"/prefs.js
     fi
 done
 IFS="${SAVEIFS}"
+
+# vim: wm=0
